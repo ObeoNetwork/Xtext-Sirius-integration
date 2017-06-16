e23cc0b461a9e355284f914e30c52d039967bf91
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-06-16 15:00:09 +0200

    basic Xtext support for aird files

---
+++ xtext-support-parent/plugins/io.typefox.xtext.sirius.aird/src/io/typefox/xtext/sirius/aird/ui/ExecutableExtensionFactory.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2017 TypeFox and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package io.typefox.xtext.sirius.aird.ui;
+
+import org.eclipse.xtext.ui.guice.AbstractGuiceAwareExecutableExtensionFactory;
+import org.osgi.framework.Bundle;
+
+import com.google.inject.Injector;
+
+/**
+ * Instantiates classes declared in the plugin.xml using the DI container.
+ * 
+ * @author koehnlein
+ */
+public class ExecutableExtensionFactory extends AbstractGuiceAwareExecutableExtensionFactory {
+
+	@Override
+	protected Bundle getBundle() {
+		return Activator.getDefault().getBundle();
+	}
+
+	@Override
+	protected Injector getInjector() {
+		return Activator.getDefault().getInjector();
+	}
+}
