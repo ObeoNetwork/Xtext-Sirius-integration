f73391171ea8e354e8dc5260f56a6a157c80db7c
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-06-20 10:53:44 +0200

    Disable resource validation for aird files

--- xtext-support-parent/plugins/io.typefox.xtext.sirius.aird/src/io/typefox/xtext/sirius/aird/AirdRuntimeModule.java
+++ xtext-support-parent/plugins/io.typefox.xtext.sirius.aird/src/io/typefox/xtext/sirius/aird/AirdRuntimeModule.java
@@ -8,6 +8,7 @@
 package io.typefox.xtext.sirius.aird;
 
 import org.eclipse.xtext.resource.generic.AbstractGenericResourceRuntimeModule;
+import org.eclipse.xtext.validation.IResourceValidator;
 
 /**
  * The DI module to configure the Xtext language services for aird models.
@@ -24,5 +25,9 @@ public class AirdRuntimeModule extends AbstractGenericResourceRuntimeModule {
 	@Override
 	protected String getFileExtensions() {
 		return "aird";
+	}
+	
+	public IResourceValidator bindIResourceValidator() {
+		return IResourceValidator.NULL;
 	}
 }
